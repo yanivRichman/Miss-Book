6934054ebbf259a8d6fccedf013e7e856715796f
Yaniv Richman <yaniv4980@gmail.com>
2021-11-09 15:34:35 +0200

    fix img

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -5,7 +5,7 @@ export default {
         <section class="about-page app-main">
             <h3 class="about-page-text" ref="header">About us: <br> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor debitis atque repellendus perferendis libero iusto eligendi quae, consequatur modi officiis veniam dolorem omnis asperiores voluptatum eaque id impedit ab rem!</h3>
             <!-- <button @click="callBus">Call the bus!</button> -->
-            <img v-bind:src="'../img/about.png'"/>
+            <img v-bind:src="'./img/about.png'"/>
         </section>
     `,
     methods: {
